3867b99a866a762f7b0c69b0d568c9b671653ec3
natmark <natmark0918@gmail.com>
2016-10-09 16:16:22 +0900

    [update] add ball motion

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+		addBall ();
+	}
+	public static void addBall(){
+		GameObject ball = Ball.AddBall (new Vector3 (0, 1.5f, -2.5f));
+		ball.GetComponent<Rigidbody> ().AddForce (Vector3.forward * 5, ForceMode.Impulse);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/court.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class court : MonoBehaviour {
+	private bool hit = false;
+	private GameObject collider;
+
+	void OnCollisionEnter(Collision collision){
+		//床にボールが落ちた時、ボールのPrefabを消して新規に作成.
+		if (collision.gameObject.tag == "Ball") {
+			hit = true;
+			collider = collision.gameObject;
+		}
+	}
+	void Update(){
+		if (hit == true) {
+			Destroy (collider);
+			GameManager.addBall ();
+			hit = false;
+		}
+	}
+}
