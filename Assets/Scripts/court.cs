708f0bf7e8dc959d2187e8a433869b1537e117e4
natmark <natmark0918@gmail.com>
2016-10-09 18:42:21 +0900

    [update] add racket bounce

--- Assets/Scripts/court.cs
+++ Assets/Scripts/court.cs
@@ -3,18 +3,18 @@ using System.Collections;
 
 public class court : MonoBehaviour {
 	private bool hit = false;
-	private GameObject collider;
+	private GameObject col;
 
 	void OnCollisionEnter(Collision collision){
 		//床にボールが落ちた時、ボールのPrefabを消して新規に作成.
 		if (collision.gameObject.tag == "Ball") {
 			hit = true;
-			collider = collision.gameObject;
+			col = collision.gameObject;
 		}
 	}
 	void Update(){
 		if (hit == true) {
-			Destroy (collider);
+			Destroy (col);
 			GameManager.addBall ();
 			hit = false;
 		}
