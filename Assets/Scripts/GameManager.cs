3a9c5fe51b6f8a7c8f8aa7ee28745e7973c834a4
natmark <natmark0918@gmail.com>
2016-10-09 19:32:45 +0900

    [update] add mBaaS
    add packeage of mBaaS
    add Test module

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using NCMB;
 
 public class GameManager : MonoBehaviour {
 	// Use this for initialization
@@ -9,8 +10,21 @@ public class GameManager : MonoBehaviour {
 	public static void addBall(){
 		GameObject ball = Ball.AddBall (new Vector3 (0, 2, -3.5f));
 		ball.GetComponent<Rigidbody> ().AddForce (new Vector3(0,1,6), ForceMode.Impulse);
+		Debug.LogWarning("addBall");   // テストでワーニングログをコール
 	}
-	
+	void OnNotificationReceived (NCMBPushPayload payload)
+	{
+		Debug.Log("OnNotificationReceived");
+		Debug.Log("PushId : " + payload.PushId);
+		Debug.Log("Massage : " + payload.Message);
+		Debug.LogWarning("OnNotificationReceived");   // テストでワーニングログをコール
+		Debug.LogWarning("PushId : " + payload.PushId);   // テストでワーニングログをコール
+		Debug.LogWarning("Massage : " + payload.Message);   // テストでワーニングログをコール
+
+		#if UNITY_ANDROID
+		Debug.Log("Title : " + payload.Title);
+		#endif
+	}
 	// Update is called once per frame
 	void Update () {
 		
